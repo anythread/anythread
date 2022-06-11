abf7a143ea31ca762dcb4c33463135d1dbcd90cd
tfius <tadej.fius@gmail.com>
2022-06-11 18:55:59 +0200

    get content

--- src/ContentView.tsx
+++ src/ContentView.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { ReactElement } from 'react'
 import * as SwarmCid from '@ethersphere/swarm-cid'
+import { Bee, Utils } from '@ethersphere/bee-js'
 
 interface Props {
   contentHash: string
+  bee: Bee
 }
 
 function getBzzLink(contentHash: string): string {
@@ -17,21 +19,34 @@ function getBzzLink(contentHash: string): string {
   }
 }
 
-export default function ContentView({ contentHash }: Props): ReactElement {
+export default function ContentView({ contentHash, bee }: Props): ReactElement {
   const [bzzLink, setBzzLink] = useState('')
+  const [text, setText] = useState('')
+  const [timestamp, setTimestamp] = useState('')
+  const [hash, setHash] = useState('')
 
   useEffect(() => {
     setBzzLink(getBzzLink(contentHash))
+    bee.downloadData(contentHash).then(data => {
+      const jsonString = new TextDecoder().decode(data)
+      const post = JSON.parse(JSON.parse(jsonString).message) // this is bad as its double encoded, content at contentHash is 2x json
+      console.log('data', data, jsonString, post)
+      setText(post.text)
+      setTimestamp(post.timestamp)
+      setHash(post.contentHash)
+    })
+    console.log('contentHash', contentHash)
   }, [contentHash])
 
   return (
     <div id={contentHash}>
-      <div>Content address: {contentHash}</div>
       <div>
-        Your content cannot be display here.
+        <small>Content address: {contentHash}</small>
+      </div>
+      <div>
+        {text}
         <br />
-        Please follow the BZZ link to get the file <br />
-        BZZ link: <a href={bzzLink}>{bzzLink}</a>
+        <a href={bzzLink}>BZZ link</a>
       </div>
     </div>
   )
